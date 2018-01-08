909e29c5954950f5221ed9fb9d79fa0722db1a35
Rajiv Ramaiah <rajivramaiah@me.com>
2018-01-08 00:07:19 -0500

    added template code for blockchain

---
+++ blockchain.py
@@ -0,0 +1,34 @@
+# Used the following tutorial: https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
