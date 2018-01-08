e5930ba74d26adb70494fa0c726b87d4859006d7
Rajiv Ramaiah <rajivramaiah@me.com>
2018-01-08 01:19:54 -0500

    added most of barebone functionality from tutorial

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,40 @@
 # Used the following tutorial: https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 
-class Blockchain:
+
+import hashlib
+import json
+from time import time
+
+
+class Blockchain(object):
     def __init__(self):
+        self.current_transactions = []
         self.chain = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -16,7 +44,6 @@ class Blockchain:
         :param amount: <int> Amount
         :return: <int> The index of the Block that will hold this transaction
         """
-
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -25,10 +52,18 @@ class Blockchain:
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        pass
-
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
